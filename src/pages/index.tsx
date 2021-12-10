ce8383bfcda93ee4429ac27d824e3404d6769a73
Desiree <desireemenezes@moovin.com.br>
2021-12-10 18:52:52 -0300

    fecth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.scss';
+
+/** client side reder */
+
+interface Post {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => { /** simula api */
+    fetch('http://localhost:3333/posts').then((response) => {
+      response.json().then((data) => {
+        setPosts(data);
+      })
+    })
+  },[]);
+
   return (
     <div>
-      <h1>Olá <s>Dev!</s></h1>
+      Posts
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
