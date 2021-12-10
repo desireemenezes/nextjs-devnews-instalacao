7d3bdc99c9984b5f8f63e3a0335b81775a85b008
Desiree <desireemenezes@moovin.com.br>
2021-12-10 17:02:35 -0300

    add link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.scss';
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.title}>Olá <s>Dev!</s></h1>
+      <h1>Olá <s>Dev!</s></h1>
     </div>
   );
 }
